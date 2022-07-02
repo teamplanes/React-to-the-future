7fa353a4ff46f3e617db59db305e94284adba01f
natbibi <natalie.wuwu@googlemail.com>
2022-07-02 14:32:31 +0100

    :sparkles: add basic search component

--- src/screens/home.js
+++ src/screens/home.js
@@ -6,19 +6,19 @@ import {
 } from '@chakra-ui/react';
 import {Nav} from '../components/bottom-nav'
 import { Logo } from '../components/logo';
+import { Search } from '../components/search';
 
 export const Home = () => {
     return (
+        <>
         <Box minH="100vh">
             <Box textAlign="center" fontSize="xl">
                 <Box spacing={8}>
-                    <Center p={20}><Logo h="40vmin" pointerEvents="none" /></Center>
-                    <Text pt={10} bg="white">
-                        HACK to the future!
-                    </Text>
+                    <Center p={20}><Search /></Center>
                 </Box>
              </Box>
             <Nav />
         </Box>
+        </>
     );
 }

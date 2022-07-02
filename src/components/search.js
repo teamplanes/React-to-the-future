55600c30e9d3faf161eb0a9466dd8d3c9ea8b3ba
natbibi <natalie.wuwu@googlemail.com>
2022-07-02 16:28:06 +0100

    :alien: create api request

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import { Input } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Input, InputGroup } from '@chakra-ui/react'
+import { useApiRequest } from '../requests'
+import { Results } from '../components/results'
 
 export const Search = () => {
+    const [inputData, setInputData] = useState('london');
+
+    const { data, error } = useApiRequest(
+        `https://api.ambeedata.com/latest/by-city?city=${inputData}`
+    );
+
+    const handleChange = (e) => {
+        setInputData(e.target.value)
+    };
+
     return (
         <>
-            <Input placeholder='Enter a City...' />
+            <InputGroup size='md'>
+                <Input
+                pr='4.5rem'
+                placeholder='Enter a City...'
+                onChange={handleChange}
+                value={inputData}
+                />
+            </InputGroup>
+
+            {data && data.slice(0, 20).map((r) => (
+                            <Results key={r.id} list={r} />
+                        ))}
+
+                        {error && <div id="error">{error}</div>}
         </>
     )
 }

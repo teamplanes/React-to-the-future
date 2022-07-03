a6c16d84e4c6af9ae806496d254004a7ef245bcb
rodages <zanasmakarov@gmail.com>
2022-07-03 09:56:41 +0100

    uploading coords

--- src/components/search.js
+++ src/components/search.js
@@ -1,25 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import {
     Box,
     Center,
 } from '@chakra-ui/react';
 import {Nav} from '../components/bottom-nav'
-import { Results } from '../components/results';
 import { Input, InputGroup, Spinner, Button } from '@chakra-ui/react'
 import axios from 'axios';
-import Search from '../components/search';
+import {Results} from './results'
 
-export const Home = () => {
-    const [inputData, setInputData] = useState('');
-    const [data, setData] = useState();
+function Search(){
+    const [inputData,setInputData ] = useState("")
+
+    const [coordinates,setCoordinates] = useState({lat:51.5085, lng:-0.1257})
+
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState();
+    const [data, setData] = useState();
+
+    console.log(coordinates,"coord")
+
+    useEffect(()=>{
+        const url= `http://api.positionstack.com/v1/forward?access_key=${process.env.REACT_APP_POSITIONSTACK_API_KEY}&query=`
+
+        async function fetchCoordinates(){
+            //need to delay loading possibility of sending request if waiting for response
+            const res = await fetch(url+inputData.replace(" ",""))
+            const data = await res.json()
+            const {longitude, latitude} = data.data[0]
+            const coordinates = {lat:latitude,lng:longitude}
+            setCoordinates(coordinates)
+        }
+        if(inputData){
+            fetchCoordinates()
+        }
+        
+    },[inputData])
 
     const handleChange = (e) => {
         setInputData(e.target.value)
     };
 
-    const handleClick = async (e) => {
+    const handleSubmit= async (e)=>{
         setLoading(true);
         e.preventDefault()
         try {
@@ -30,31 +51,34 @@ export const Home = () => {
                     "Content-Type": `application/json;charset=utf-8`
                 }
             };
-            const {data} = await axios.get(`https://api.ambeedata.com/latest/by-city?city=${inputData}`, options)
+            const {data} = await axios.get(`https://api.ambeedata.com/latest/by-lat-lng?lat=${coordinates.lat}&lng=${coordinates.lng}`, options)
             
             if (data.err) {
                 throw new Error(data.err)
             }
             setData(data.stations[0])
-            console.log(data.stations[0])
             setLoading(false);
             setInputData('')
-            localStorage.setItem("lng", data.stations[0].lng);
-            localStorage.setItem("lat", data.stations[0].lat);
+            // localStorage.setItem("lng", data.stations[0].lng);
+            // localStorage.setItem("lat", data.stations[0].lat);
+
         } catch {
             console.warn("There's an error!!! Cannot fetch data!")
             setError('Oops, please try again!')
             setLoading(false);
         }
     }
+
+    // fetchCoordinates("se165ud", setCoordinates)
     return (
         <>
-        <Search />
-            {/* <Box minH="100vh">
+            <Box minH="100vh">
                 <Box textAlign="center" fontSize="xl">
                     <Box spacing={8}>
                         <Center p={60}>
-                            <form onSubmit={handleClick}>
+                            <form 
+                            onSubmit={handleSubmit}
+                            >
                                 <InputGroup size='md'>
                                     <Input
                                         pr='4.5rem'
@@ -71,7 +95,9 @@ export const Home = () => {
                 </Box>
                 {data ? <Results data={data} /> : ""}
                 <Nav />
-            </Box> */}
+            </Box>
         </>
     );
 }
+
+export default Search
